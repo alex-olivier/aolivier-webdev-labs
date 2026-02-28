f30f54a4e243f251656fab127424fe4e47aa0c3e
aolivier <aolivier34@gmail.com>
2026-02-27 17:18:47 -0800

    08-jquery exercises

--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -1,52 +1,34 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = 'https://anapioficeandfire.com/api/books?pageSize=50';
 
-const app = document.querySelector('#books');
-app.style.paddingLeft = 0;
-const loading = document.querySelector('#loading');
+const $books = $('#books');
+const $loading = $('#loading');
 
 const addBookToDOM = (item) => {
-  console.log(item);
-  let element = document.createElement('div');
-  let title = document.createElement('h4');
-  let author = document.createElement('p');
-  let published = document.createElement('p');
-  let pages = document.createElement('p');
+  const $element = $('<div>').addClass('d-flex flex-column align-items-center mt-4');
+  const $title = $('<h4>').text(item.name);
+  const $author = $('<p>').text(`by ${item.authors[0]}`);
+  const $published = $('<p>').text(item.released.slice(0, 4));
+  const $pages = $('<p>').text(`${item.numberOfPages} pages`);
 
-  element.style.display = 'flex';
-  element.style.flexDirection = 'column';
-  element.style.alignItems = 'center';
-  element.style.marginTop = '20px';
-
-  title.textContent = item.name;
-  author.textContent = `by ${item.authors[0]}`;
-  published.textContent = item.released.substr(0, 4);
-  pages.textContent = `${item.numberOfPages} pages`;
-
-  element.append(title);
-  element.append(author);
-  element.append(published);
-  element.append(pages);
-
-  app.append(element);
+  $element.append($title, $author, $published, $pages);
+  $books.append($element);
 };
 
-const fetchData = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-        addBookToDOM(item);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      let li = document.createElement('li');
-      li.textContent = `An error occured. Please try again.`;
-      app.append(li);
-    })
-    .finally(() => {
-      app.removeChild(loading);
+$.ajax({
+  url: url,
+  method: 'GET',
+  dataType: 'json',
+})
+  .done((data) => {
+    data.forEach((item) => {
+      addBookToDOM(item);
     });
-};
-
-fetchData(url);
+  })
+  .fail((xhr, status, error) => {
+    console.log(status, error, xhr);
+    const $error = $('<p>').text('An error occurred. Please try again.');
+    $books.append($error);
+  })
+  .always(() => {
+    $loading.remove();
+  });
